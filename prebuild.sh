cd6749153fc33dee909423caa67f07af56b1651b
stsdc <stanislaw.dac@gmail.com>
2018-08-19 00:01:25 +0200

    🌈 Add colors

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# Font styles
+GREEN="\e[1;38;5;40m"
+BOLD="\e[1m"
+RST="\e[0m"
+
 export IMG_NAME='turtleos'
 export DEBIAN_FRONTEND='noninteractive'
 export LC_ALL='C.UTF-8'
@@ -13,26 +18,32 @@ mkdir -p build
 cd build
 
 merge () {
-    # Merge overlay with original pi-gen
+    # Merge overlay with the original pi-gen
+    echo -e " Merging overlay with the original pi-gen"
     cp -rf ../pi-gen/* .
     rm -rf stage2/EXPORT_NOOBS stage2/EXPORT_IMAGE stage3 stage4 stage5
     cp -rf ../pi-gen-overlay/* .
 }
 
 case "$1" in
-    -s) echo "Skipping all stages except stage3"
+    -s) echo -e "$BOLD Performing build of $GREEN$IMG_NAME$RST..."
+        echo -e "$BOLD Skipping all stages except stage3$RST"
 
         merge
 
         for STAGE_DIR in "stage"*; do
             if [ "$STAGE_DIR" != stage3 ]; then
-                echo "+ Adding SKIP file to $STAGE_DIR"
+                echo " + Adding SKIP file to $STAGE_DIR"
                 touch $STAGE_DIR/SKIP
             fi
         done
+        echo -en " "
+        for i in {76..81} {81..76} ; do echo -en "\e[38;5;${i}m#\e[0m" ; done ; echo
+
         CLEAN=1 ./build.sh
         ;;
-    *) echo "Performing full build of $IMG_NAME..."
+    *) echo -e "$BOLD Performing build of $GREEN$IMG_NAME$RST..."
+       echo -e "$BOLD Building all stages except$RST"
 
         merge
 
