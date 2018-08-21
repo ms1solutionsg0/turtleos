38014610ebcc53ff91c7af008491d0015ad5bf3c
stsdc <stanislaw.dac@gmail.com>
2018-08-21 10:56:56 +0200

    do not save deb to tmp

--- pi-gen-overlay/stage3/03-install-packages/00-run.sh
+++ pi-gen-overlay/stage3/03-install-packages/00-run.sh
@@ -2,10 +2,10 @@
 
 on_chroot <<-EOF
     echo "=> Retrieving latest TCS..."
-    curl -L $(curl --silent 'https://api.github.com/repos/TurtleRover/tcs/releases/latest' | jq '.assets[0].browser_download_url') --output /tmp/tcs.deb
+    curl -L $(curl --silent 'https://api.github.com/repos/TurtleRover/tcs/releases/latest' | jq '.assets[0].browser_download_url') --output tcs.deb
 
-    apt install /tmp/tcs.deb
+    apt install tcs.deb
 
     echo "=> Removing deb..."
-    rm /tmp/tcs.deb
+    rm tcs.deb
 EOF
