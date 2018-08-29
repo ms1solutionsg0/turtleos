4913d601cea8a4c73fc0c4ce8ffcd2dffdb4197d
stsdc <stanislaw.dac@gmail.com>
2018-08-29 13:43:47 +0200

    don't show progressbar when retrieving tcs

--- pi-gen-overlay/stage3/10-install-tcs/00-run-chroot.sh
+++ pi-gen-overlay/stage3/10-install-tcs/00-run-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "=> Retrieving latest TCS..."
-wget $(wget -q -O -  'https://api.github.com/repos/TurtleRover/tcs/releases/latest' | jq -r '.assets[0].browser_download_url') -O ./tcs.deb
+wget -q $(wget -q -O -  'https://api.github.com/repos/TurtleRover/tcs/releases/latest' | jq -r '.assets[0].browser_download_url') -O ./tcs.deb
 
 apt install ./tcs.deb
 
