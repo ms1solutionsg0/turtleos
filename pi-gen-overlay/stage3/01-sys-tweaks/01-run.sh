cb176dc2151018946145afbf3fc36501b8b87b81
stsdc <stanislaw.dac@gmail.com>
2018-09-21 13:47:49 +0200

    add custom motd #3

---
+++ pi-gen-overlay/stage3/01-sys-tweaks/01-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+rm /etc/motd
+rm /etc/update-motd.d/10-uname
+EOF
+
+install -m 755 files/etc/turtleos_version "$ROOTFS_DIR"/etc/
+install -m 755 files/etc/update-motd.d/* "$ROOTFS_DIR"/etc/update-motd.d/
+
+DATE=$(date +"%Y%m%d")
+sed "/BUILD_ID=/$/ $DATE/" "$ROOTFS_DIR"/etc/turtleos_version
