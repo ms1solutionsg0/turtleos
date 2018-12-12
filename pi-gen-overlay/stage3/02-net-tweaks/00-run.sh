72e0e8313076671bf487bb35b721fd83ca83e66d
Błażej Sowa <bsowa123@gmail.com>
2018-12-12 16:42:35 +0100

    remove wpa_supplicant configuration file

--- pi-gen-overlay/stage3/02-net-tweaks/00-run.sh
+++ pi-gen-overlay/stage3/02-net-tweaks/00-run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-install -v -m 644 files/etc/wpa_supplicant/*.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 install -v -m 644 files/etc/*.conf	"${ROOTFS_DIR}/etc/"
 install -v -m 644 files/etc/hostapd/hostapd.conf	"${ROOTFS_DIR}/etc/hostapd/"
 
